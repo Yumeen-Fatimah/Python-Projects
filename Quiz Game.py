730fd78bb38d07f8e15160eb05f396d1c51a1c3b
Yumeen-Fatimah <130150256+Yumeen-Fatimah@users.noreply.github.com>
2023-04-16 01:53:02 +0500

    Add files via upload

---
+++ Quiz Game.py
@@ -0,0 +1,39 @@
+questions = ["What does RAM stands for?","What does ROM stands for","What is not the part of CPU?"]
+op1 = ["a.Random Access Memory","b.Read only Memory","c.Required as Memory"]
+op2 =["a.Random Access Memory","b.Read only Memory","c.Rwite only Memory"]
+op3 = ["a.ALU","b.RCU","c.CU"]
+options = [op1,op2,op3]
+ans1 = 'a'
+ans2 = 'b'
+ans3 = 'b'
+answers = [ans1,ans2,ans3]
+score = 0
+print("___________________________________________________________________")
+print("___________________________________________________________________")
+for i in range(0,3):
+    print(questions[i]+"\n")
+    print(options[i])
+    answer = input("")
+    
+    if(answer == answers[i]):
+        print("You got it!")
+        score += 1
+        print("------------------------------------------------------------------------")
+        continue
+    else:
+        j = 0
+        while(j <4):
+            print("Try again")
+            print(questions[i]+"\n")
+            print(options[i])
+            answer = input("")
+            if(answer == answers[i]):
+                print("You got it!")
+                score += 1 
+                break  
+            else:
+                j=j+1
+                continue
+        continue
+print("Your score is "+str(score))        
+        
